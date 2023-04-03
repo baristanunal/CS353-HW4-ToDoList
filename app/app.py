0a54f466b4dfc430f215527e179d5b6e3b5854b1
baristanunal <baristunal45@gmail.com>
2023-04-04 00:28:00 +0300

    Implemented add and delete task

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 
 import re
 import os
+import datetime
 
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
@@ -69,14 +70,57 @@ def register():
 
 @app.route('/tasks', methods =['GET', 'POST'])
 def tasks():
-
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
 
-    cursor.execute('SELECT * FROM Task WHERE user_id = % s', (session['userid'],))
-    users_tasks = cursor.fetchall()
-    print(users_tasks)
+    cursor.execute('SELECT * FROM TodoTask WHERE user_id = % s', (session['userid'],))
+    todo_tasks = cursor.fetchall()
 
-    return render_template('tasks.html', users_tasks=users_tasks)
+    cursor.execute('SELECT * FROM DoneTask WHERE user_id = % s', (session['userid'],))
+    done_tasks = cursor.fetchall()
+
+    return render_template('tasks.html', todo_tasks=todo_tasks, done_tasks=done_tasks,)
+
+@app.route('/add_task', methods =['GET', 'POST'])
+def add_task():
+    message = ''
+
+    if request.method == 'POST' and 'title' in request.form and 'description' in request.form and 'deadline' in request.form and 'task_type' in request.form:
+        title = request.form['title']
+        description = request.form['description']
+        deadline = request.form['deadline']
+        creation_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        user_id = session['userid']
+        task_type = request.form['task_type']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+        if not title or not description or not deadline or not task_type:
+            message = 'Please fill out the form!'
+
+        else:
+            cursor.execute('INSERT INTO ActiveTask (id, title, description, deadline, creation_time, '
+                          'user_id, task_type) VALUES (NULL, % s, % s, % s, % s, % s, % s )',
+                          (title, description, deadline, creation_time, user_id, task_type, ))
+            mysql.connection.commit()
+            message = 'Task successfully created!'
+
+    elif request.method == 'POST':
+        message = 'Please fill all the fields!'
+
+    return render_template('addTask.html', message=message)
+
+@app.route('/delete_task', methods =['GET', 'POST'])
+def delete_task():
+    message = ''
+
+    if request.method == 'POST' and 'del_id' in request.form:
+        task_id = request.form['del_id']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('DELETE FROM TodoTask WHERE ( id = % s ) ', task_id)
+        mysql.connection.commit()
+        message = 'Task successfully deleted!'
+
+    return redirect(url_for('tasks'))
+
 
 @app.route('/analysis', methods =['GET', 'POST'])
 def analysis():
--- schema.sql
+++ schema.sql
@@ -1,37 +1,50 @@
 CREATE DATABASE IF NOT EXISTS cs353hw4db;
 USE cs353hw4db;
 
+DROP TABLE TodoTask;
+DROP TABLE DoneTask;
 DROP TABLE User;
-DROP TABLE Task;
 DROP TABLE TaskType;
 
 CREATE TABLE User(
-	id int,
+	id int NOT NULL AUTO_INCREMENT,
     password varchar(255),
     username varchar(255),
     email varchar(255),
-    primary key (id)
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE Task(
-	id int,
+CREATE TABLE TodoTask(
+	id int NOT NULL AUTO_INCREMENT,
     title varchar(255),
     description text,
-    status varchar(255),
+    deadline datetime,
+    creation_time datetime,
+    user_id int,
+    task_type varchar(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES User (id)
+);
+
+CREATE TABLE DoneTask(
+	id int NOT NULL,
+    title varchar(255),
+    description text,
     deadline datetime,
     creation_time datetime,
     done_time datetime,
     user_id int,
     task_type varchar(255),
-    primary key (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES User (id)
 );
 
 CREATE TABLE TaskType(
 	type varchar(255),
-    primary key (type)
+    PRIMARY KEY (type)
 );
 
-INSERT INTO User VALUES( 
+INSERT INTO User VALUES(
 	'1',
     'pass123',
     'user1',
@@ -65,11 +78,10 @@ INSERT INTO User VALUES(
 	'Hobbies'
  );
  
- INSERT INTO Task VALUES(
+ INSERT INTO DoneTask VALUES(
 	'1',
     'Go for a walk',
     'Walk for at least 30 mins',
-    'Done',
     '2023-03-20 17:00:00',
     '2023-03-15 10:00:00',
     '2023-03-20 10:00:00',
@@ -77,11 +89,10 @@ INSERT INTO User VALUES(
     'Health'
  );
  
- INSERT INTO Task VALUES(
+ INSERT INTO DoneTask VALUES(
 	'2',
     'Clean the house',
     'Clean the whole house',
-    'Done',
     '2023-03-18 12:00:00',
     '2023-03-14 09:00:00',
     '2023-03-18 17:00:00',
@@ -89,35 +100,30 @@ INSERT INTO User VALUES(
     'Lifestyle'
  );
  
-  INSERT INTO Task VALUES(
+  INSERT INTO TodoTask VALUES(
 	'3',
     'Submit report',
     'Submit quarterly report',
-    'Todo',
     '2023-04-12 17:00:00',
     '2023-03-21 10:00:00',
-    null,
     '1',
     'Job'
  );
  
-  INSERT INTO Task VALUES(
+  INSERT INTO TodoTask VALUES(
 	'4',
     'Call Mom',
     'Call Mom and wish her',
-    'Todo',
     '2023-04-06 11:00:00',
     '2023-03-23 12:00:00',
-    null,
     '1',
     'Family'
  );
  
-  INSERT INTO Task VALUES(
+  INSERT INTO DoneTask VALUES(
 	'5',
     'Gym workout',
     'Do weight training for an hour',
-    'Done',
     '2023-03-19 14:00:00',
     '2023-03-12 10:00:00',
     '2023-03-19 11:00:00',
@@ -125,23 +131,20 @@ INSERT INTO User VALUES(
     'Health'
  );
  
- INSERT INTO Task VALUES(
+ INSERT INTO TodoTask VALUES(
 	'6',
     'Play guitar',
     'Learn new song for an hour',
-    'Todo',
     '2023-04-05 20:00:00',
     '2023-03-20 14:00:00',
-    null,
     '2',
     'Hobbies'
  );
  
-  INSERT INTO Task VALUES(
+  INSERT INTO DoneTask VALUES(
 	'7',
     'Book flights',
     'Book flights for summer vacation',
-    'Done',
     '2023-03-16 09:00:00',
     '2023-03-13 13:00:00',
     '2023-03-16 11:00:00',
@@ -149,35 +152,30 @@ INSERT INTO User VALUES(
     'Lifestyle'
  );
  
-  INSERT INTO Task VALUES(
+  INSERT INTO TodoTask VALUES(
 	'8',
     'Write a blog post',
     'Write about recent project',
-    'Todo',
     '2023-04-11 17:00:00',
     '2023-03-22 09:00:00',
-    null,
     '2',
     'Job'
  );
  
- INSERT INTO Task VALUES(
+ INSERT INTO TodoTask VALUES(
 	'9',
     'Grocery shopping',
     'Buy groceries for the week',
-    'Todo',
     '2023-04-05 18:00:00',
     '2023-03-31 10:00:00',
-    null,
     '2',
     'Family'
  );
  
- INSERT INTO Task VALUES(
+ INSERT INTO DoneTask VALUES(
 	'10',
     'Painting',
     'Paint a landscape for 2 hours',
-    'Done',
     '2023-03-23 15:00:00',
     '2023-03-18 14:00:00',
     '2023-03-23 16:00:00',
